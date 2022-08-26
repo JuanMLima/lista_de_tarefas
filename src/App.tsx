26b3604b5049890fcea1cd8992fae4de874d49f8
Juan Menezes <99377147+JuanMLima@users.noreply.github.com>
2022-08-26 01:03:47 -0300

    Incluindo inputs

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,30 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 const App = () => {
     const [list, setList] = useState<Item[]>([
-        { id: 1, name: 'comprar o pão na padaria', done: false },
-        { id: 2, name: 'Comprar leite', done: true },
-
+    
     ]);
+
+    const handleAddTask = (TaskName: string) =>{
+        let newList= [...list];
+        newList.push({
+            id: list.length+1,
+            name:TaskName,
+            done:false,
+        });
+
+        setList(newList);
+
+    }
 
     return (
         <C.Container>
             <C.Area>
                 <C.Header>Lista de tarefas</C.Header>
 
-                {/* Área para adicionar nova tarefa */}
+                <AddArea onEnter={handleAddTask}/>
 
                 {list.map((item, index)=>(
                     <ListItem key={index} item={item} />
